ea77196d172a875f70c4b1cded788bd40b2d726e
Fwiffo <borgified@gmail.com>
2013-05-26 16:21:39 -0600

    Update grind.sh

--- grind.sh
+++ grind.sh
@@ -26,4 +26,5 @@ export G_MESSAGES_DEBUG G_SLICE
 GEN=--gen-suppressions=all
 GEN="--gen-suppressions=no --num-callers=50 --read-var-info=yes"
 GEN="--gen-suppressions=all --num-callers=50"
-sudo strace -ttT valgrind -q --sim-hints=lax-ioctls --leak-check=full --show-reachable=yes --suppressions=$HERE/valgrind-msgs.supp $GEN --error-exitcode=100 --trace-children=no --child-silent-after-fork=yes ./mainlooptest $REPCOUNT
+#sudo strace -ttT valgrind -q --sim-hints=lax-ioctls --leak-check=full --show-reachable=yes --suppressions=$HERE/valgrind-msgs.supp $GEN --error-exitcode=100 --trace-children=no --child-silent-after-fork=yes ./mainlooptest $REPCOUNT
+valgrind -q --sim-hints=lax-ioctls --leak-check=full --show-reachable=yes --suppressions=$HERE/valgrind-msgs.supp $GEN --error-exitcode=100 --trace-children=no --child-silent-after-fork=yes ./mainlooptest $REPCOUNT
